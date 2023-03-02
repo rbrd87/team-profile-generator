f6b2bddf719f9be5c03494b36a4cbd3281fb99ec
rbrd87 <rochellembradley@gmail.com>
2023-03-02 16:18:36 +0000

    Refactors questions to use DRY method

--- index.js
+++ index.js
@@ -15,69 +15,92 @@ const render = require("./src/page-template.js");
 const employees = [];
 
 // Gathering information about the development team members, and rendering the HTML file.
-const managerQuestions = [
-    {
-      type: "input",
-      name: "name",
-      message: "What is the team member's name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the team member's ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the team member's email address?",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "What is the manager's office number?",
-    },
-  ];
-  const engineerQuestions = [
-    {
-      type: "input",
-      name: "name",
-      message: "What is the team member's name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the team member's ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the team member's email address?",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is the team member's GitHub username?",
-    },
-  ];
-  const internQuestions = [
-    {
-      type: "input",
-      name: "name",
-      message: "What is the team member's name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the team member's ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the team member's email address?",
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "What school does this team member go to?",
-    },
-  ];
+const nameQuestion = {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+    validate(name) {
+        if (!/[a-zA-Z]+$/.test(name)) {
+            return "Please only use letters";
+        }
+        return true
+    }
+};
+
+const idQuestion = {
+    type: "input",
+    message: "What is your employee ID?",
+    name: "name",
+    validate(id) {
+        if (/[a-zA-Z]+$/.test(id)) {
+            return "Please only use numbers";
+        }
+        return true
+    }
+};
+
+const emailQuestion = {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+    validate(email) {
+        //simple validation, as emails are super complicated to validate
+        if (!/[@]/.test(email) || !/[.]/.test(email)) {
+            return "Please enter a valid email address"
+        }
+        return true
+    }
+};
+
+const officeQuestion = {
+    type: "input",
+    message: "What is your office number?",
+    name: "office",
+    validate(office) {
+        if (/[a-zA-Z]+$/.test(office)) {
+            return "Please only use numbers"
+        }
+        return true
+    }
+};
+
+const githubQuestion = {
+    type: "input",
+    message: "What is your github username?",
+    name: "github",
+    validate(github) {
+        if (!/[a-zA-Z]+$/.test(github)) {
+            return "Please only use letters"
+        }
+        return true
+    }
+};
+
+const schoolQuestion = {
+    type: "input",
+    message: "What school do you attend?",
+    name: "school",
+    validate(school) {
+        if (!/[a-zA-Z]+$/.test(school)) {
+            return "Please only use letters"
+        }
+        return true
+    }
+};
+
+// Function to store the question answers and push to the employees array
+askManagerQuestions = () => {
+    inquirer
+        .prompt([nameQuestion, idQuestion, emailQuestion, officeQuestion])
+        .then((response) => {
+            const theManager = new Manager(response.name, response.id, response.email, response.office);
+            employees.push(theManager);
+        });
+}
+
+// init function that calls manager func
+function init() {
+    askManagerQuestions();
+}
+
+init();
