ee855df8d625c2f6dd7ed402dbaf998bbab80158
rbrd87 <rochellembradley@gmail.com>
2023-03-02 15:29:22 +0000

    Added an empty array to store the employees and added some questions to index.js

--- index.js
+++ index.js
@@ -11,4 +11,73 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+// Empty array to store the employees
+const employees = [];
+
 // Gathering information about the development team members, and rendering the HTML file.
+const managerQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team member's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team member's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team member's email address?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the manager's office number?",
+    },
+  ];
+  const engineerQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team member's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team member's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team member's email address?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is the team member's GitHub username?",
+    },
+  ];
+  const internQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team member's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team member's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team member's email address?",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "What school does this team member go to?",
+    },
+  ];
